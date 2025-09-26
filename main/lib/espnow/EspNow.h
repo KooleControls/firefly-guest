4f3c0df7f3e29160df671b686d961e290d4af044
Bas Visscher <bas@koolecontrols.nl>
2025-09-26 14:58:18 +0200

    Moved some files to the lib

--- main/lib/espnow/EspNow.h
+++ main/lib/espnow/EspNow.h
@@ -14,22 +14,22 @@ class EspNow
     static constexpr const char *TAG = "EspNow";
 
 public:
-    static constexpr size_t CommandSize = 4;          // raw bytes in frame
-    static constexpr size_t DataSize = 16;            // payload bytes
+    static constexpr size_t COMMAND_SIZE = 4;               // raw bytes in frame
+    static constexpr size_t MAX_DATA_SIZE = 16;             // payload bytes
 
 private:
     struct Frame
     {
         uint8_t destination[ESP_NOW_ETH_ALEN];
-        uint8_t commandId[CommandSize];   // raw bytes, no null terminator
-        uint8_t data[DataSize];
+        uint8_t commandId[COMMAND_SIZE];   // raw bytes, no null terminator
+        uint8_t data[MAX_DATA_SIZE];
     } __attribute__((packed));
 
 public:
     struct Package
     {
-        char commandId[CommandSize + 1]; // C-string (4 chars + '\0')
-        uint8_t data[DataSize];
+        char commandId[COMMAND_SIZE + 1]; // C-string (4 chars + '\0')
+        uint8_t data[MAX_DATA_SIZE];
         size_t dataSize{};
         uint8_t source[ESP_NOW_ETH_ALEN]{};
         uint8_t destination[ESP_NOW_ETH_ALEN]{};
@@ -37,10 +37,10 @@ public:
         bool isForMe{false};
 
         bool isOnlyForMe() const { return isForMe && !isBroadcast; }
-        static Package Make(const uint8_t *destination, const char commandId[CommandSize]);
+        static Package Make(const uint8_t *destination, const char commandId[COMMAND_SIZE]);
 
         template<typename T>
-        static Package Make(const uint8_t *destination, const char commandId[CommandSize], const T& data);
+        static Package Make(const uint8_t *destination, const char commandId[COMMAND_SIZE], const T& data);
 
         template<typename T>
         bool GetData(T &out) const;
@@ -73,13 +73,13 @@ inline EspNow *EspNow::instance = nullptr;
 
 
 // Make with no payload
-inline EspNow::Package EspNow::Package::Make(const uint8_t *destination, const char commandId[EspNow::CommandSize])
+inline EspNow::Package EspNow::Package::Make(const uint8_t *destination, const char commandId[EspNow::COMMAND_SIZE])
 {
     EspNow::Package pkg{};
     memcpy(pkg.destination, destination, ESP_NOW_ETH_ALEN);
 
-    memcpy(pkg.commandId, commandId, EspNow::CommandSize);
-    pkg.commandId[EspNow::CommandSize] = '\0';
+    memcpy(pkg.commandId, commandId, EspNow::COMMAND_SIZE);
+    pkg.commandId[EspNow::COMMAND_SIZE] = '\0';
 
     pkg.dataSize = 0;
     pkg.isBroadcast = (memcmp(destination, "\xFF\xFF\xFF\xFF\xFF\xFF", ESP_NOW_ETH_ALEN) == 0);
@@ -90,13 +90,13 @@ inline EspNow::Package EspNow::Package::Make(const uint8_t *destination, const c
 
 // Make with struct payload
 template <typename T>
-inline EspNow::Package EspNow::Package::Make(const uint8_t *destination, const char commandId[EspNow::CommandSize], const T &value)
+inline EspNow::Package EspNow::Package::Make(const uint8_t *destination, const char commandId[EspNow::COMMAND_SIZE], const T &value)
 {
     static_assert(std::is_trivially_copyable<T>::value,
                   "T must be trivially copyable");
 
     Package pkg = Make(destination, commandId);
-    pkg.dataSize = std::min(sizeof(T), DataSize);
+    pkg.dataSize = std::min(sizeof(T), MAX_DATA_SIZE);
     memcpy(pkg.data, &value, pkg.dataSize);
     return pkg;
 }
@@ -108,9 +108,10 @@ inline bool EspNow::Package::GetData(T &out) const
     static_assert(std::is_trivially_copyable<T>::value,
                   "T must be trivially copyable");
 
-    if (dataSize != sizeof(T))
+    if (dataSize < sizeof(T))
         return false;
-
+    
     memcpy(&out, data, sizeof(T));
     return true;
 }
+
